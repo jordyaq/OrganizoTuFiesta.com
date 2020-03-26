013ba186ae0ddfe1909dd2a0458ba1c59ebfb295
jordyaq <jordyaq@outlook.com>
2020-03-25 21:57:05 -0700

    mobile 800px

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <body>
 
     
-    <a href="https://api.whatsapp.com/send?phone=5534160848&text=%BFHola%20me%20puedes%20dar%20mas%20informaci%F3n%3F" id="whatsapp"><img src="images/whats-contacto.png" alt="Link mensaje whatsapp" width="80px" height="80px"></a>
+    <a href="https://api.whatsapp.com/send?phone=525534160848&text=%BFHola%20me%20puedes%20dar%20mas%20informaci%F3n%3F" id="whatsapp"><img src="images/whats-contacto.png" alt="Link mensaje whatsapp" width="80px" height="80px"></a>
 
     <header>
         <div class="menu">
